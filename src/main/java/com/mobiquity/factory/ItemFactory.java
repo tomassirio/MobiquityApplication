05da01ea662b916fd5d01660fded5a609e809a35
Tomas Sirio <tomassirio@gmail.com>
2021-05-24 17:23:57 -0300

    Separated package into a new project

--- src/main/java/com/mobiquity/factory/ItemFactory.java
+++
@@ -1,8 +0,0 @@
-package com.mobiquity.factory;
-
-import com.mobiquity.model.ItemDTO;
-
-public class ItemFactory {
-
-    public static ItemDTO createItem(Integer id, Double weight, Integer value) {return new ItemDTO(id, weight, value);}
-}
